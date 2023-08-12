30ad6fbf18e1c7a860368b44ead65369013e2acf
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2023-08-12 10:56:11 +0200

    added listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:learneso/database_helper.dart';
 import './word.dart';
 
 void main() {
@@ -47,6 +48,8 @@ class _MyAppState extends State<MyApp> {
           )),
           if (_selectedView == 'Translator')
             const MaterialPage(child: TranslatorView())
+          else if (_selectedView == 'WordsList')
+            const MaterialPage(child: WordsListView())
         ],
         onPopPage: (route, result) {
           setSelectedView(null);
@@ -216,6 +219,62 @@ class _TranslatorViewState extends State<TranslatorView> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class WordsListView extends StatefulWidget {
+  const WordsListView({super.key});
+
+  @override
+  State<WordsListView> createState() => _WordsListViewState();
+}
+
+class _WordsListViewState extends State<WordsListView> {
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        foregroundColor: Colors.white,
+        title: const Text('Lista fiszek'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Word>>(
+            future: DatabaseHeloper.instance.getWords(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Word>> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: Text('Wczytywanie...'),
+                );
+              }
+
+              return snapshot.data!.isEmpty
+                  ? const Center(
+                      child: Text('Brak fiszek'),
+                    )
+                  : ListView(
+                      children: snapshot.data!
+                          .map(
+                            (e) => Center(
+                              child: ListTile(
+                                title: Text(e.original),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    );
+            }),
       ),
     );
   }
